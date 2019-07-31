7441e3ca3d3356cbd3c8561bfb24a2334fb3908f
Aleksey Kravetz <alex@alekseys-mbp.mynet>
2019-07-31 18:38:37 +0300

    added lifecycle

--- App.js
+++ App.js
@@ -4,12 +4,42 @@ import { StyleSheet, Text, View } from 'react-native';
 import MyButton from './CounterButton';
 
 
+
+class Blink extends React.Component {
+
+  componentDidMount(){
+    // Toggle the state every second
+    setInterval(() => (
+      this.setState(previousState => (
+        { isShowingText: !previousState.isShowingText }
+      ))
+    ), 1000);
+  }
+
+  //state object
+  state = { isShowingText: true };
+
+  render() {
+    if (!this.state.isShowingText) {
+      return null;
+    }
+
+    return (
+      <Text>{this.props.text}</Text>
+    );
+  }
+}
+
+
+
 export default class MyApp extends React.Component {
   render() {
     return(
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
         <MyButton />
+        <Blink text='I love to blink' />
+
       </View>
     );
   }
