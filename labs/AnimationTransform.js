c6bfa270c1f5e95db10650862c6ab93300952acc
Aleksey Kravetz <alex@alekseys-mbp.mynet>
2019-08-14 11:56:23 +0300

    added more animation axamples

--- labs/AnimationTransform.js
+++ labs/AnimationTransform.js
@@ -7,20 +7,31 @@ export default class AnimationTransform extends Component {
         super();
         this.spinValue = new Animated.Value(0);
         this.widthValue = new Animated.Value(227);
+        this.colorValue = new Animated.Value(0);
     }
 
     componentDidMount() {
-        this.spin();
+        this.animateSpin();
         this.animateWidthShrink();
+        this.animateColor();
     }
 
-    spin() {
+    animateSpin() {
         this.spinValue.setValue(0);
         Animated.timing(this.spinValue, {
             toValue: 1,
             duration: 4000,
             easing: Easing.linear
-        }).start(() => this.spin());
+        }).start(() => this.animateSpin());
+    }
+
+    animateColor() {
+        this.colorValue.setValue(0);
+        Animated.timing(this.colorValue, {
+            toValue: 1,
+            duration: 4000,
+            easing: Easing.linear
+        }).start(() => this.animateColor());
     }
 
     animateWidthShrink() {
@@ -45,20 +56,23 @@ export default class AnimationTransform extends Component {
             outputRange: ['0deg', '360deg']
         });
 
+        const color = this.colorValue.interpolate({
+            inputRange: [0, 1],
+            outputRange: ['red', 'green']
+        });
+
         return (
-            <View style={styles.container}>
-                <Animated.Image
-                    style={{ width: this.widthValue, height: 200, transform: [{ rotate: spin }] }}
-                    source={{ uri: 'https://s3.amazonaws.com/media-p.slid.es/uploads/alexanderfarennikov/images/1198519/reactjs.png' }}
-                />
-            </View>
+                <Animated.View style={{ ...styles.container, backgroundColor: color }}>
+                    <Animated.Image
+                        style={{ width: this.widthValue, height: 200, transform: [{ rotate: spin }] }}
+                        source={{ uri: 'https://s3.amazonaws.com/media-p.slid.es/uploads/alexanderfarennikov/images/1198519/reactjs.png' }}
+                    />
+                </Animated.View>
         );
     }
 
 }
 
 const styles = StyleSheet.create({
-    container: { flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#F5FCFF' },
-    welcome: { fontSize: 20, textAlign: 'center', margin: 10 },
-    instructions: { textAlign: 'center', color: '#333333', marginBottom: 5 }
+    container: { flex: 1, justifyContent: 'center', alignItems: 'center' },
 });
